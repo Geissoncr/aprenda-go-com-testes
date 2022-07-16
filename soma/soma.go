21c988be5f9b3b56050c044e6e8e849d58e35bb4
geissoncr <geissoncr@gmail.com>
2022-07-16 09:59:56 -0300

    arrays e slices

--- soma/soma.go
+++ soma/soma.go
@@ -1,10 +1,31 @@
 package soma
 
-func Soma(numeros [5]int) int {
+func Soma(numeros []int) int {
 	soma := 0
 
 	for _, numero := range numeros {
 		soma += numero
 	}
 	return soma
+}
+
+func SomaTudo(slices ...[]int) (somas []int) {
+
+	for _, numeros := range slices {
+		somas = append(somas, Soma(numeros))
+	}
+	return somas
+}
+
+func SomaTodoOResto(slices ...[]int) (somas []int) {
+
+	for _, numeros := range slices {
+		if len(numeros) == 0 {
+			somas = append(somas, 0)
+		} else {
+			resto := numeros[1:]
+			somas = append(somas, Soma(resto))
+		}
+	}
+	return somas
 }
--- soma/soma_test.go
+++ soma/soma_test.go
@@ -1,14 +1,54 @@
 package soma
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSoma(t *testing.T) {
-	numeros := [5]int{1, 2, 3, 4, 5}
+	t.Run("Coleção com qq tamanho de numero", func(t *testing.T) {
+		numeros := []int{1, 2, 3}
+		resultado := Soma(numeros)
+		esperado := 6
 
-	resultado := Soma(numeros)
-	esperado := 15
+		if esperado != resultado {
+			t.Errorf("Resultado esperado '%d', veio '%d' entrada '%v'", esperado, resultado, numeros)
+		}
+	})
+}
+func TestSomaTudo(t *testing.T) {
+	t.Run("Coleção de slices", func(t *testing.T) {
 
-	if esperado != resultado {
-		t.Errorf("Resultado esperado '%d', veio '%d' entrada '%v'", esperado, resultado, numeros)
+		resultado := SomaTudo([]int{1, 2, 3}, []int{1, 2})
+		esperado := []int{6, 3}
+
+		if !reflect.DeepEqual(resultado, esperado) {
+			t.Errorf("Resultado esperado '%d', veio '%d' entrada", esperado, resultado)
+		}
+	})
+}
+
+func TestSomaTodoOResto(t *testing.T) {
+
+	verificaSomas := func(t *testing.T, resultado, esperado []int) {
+		t.Helper()
+		if !reflect.DeepEqual(resultado, esperado) {
+			t.Errorf("resultado %v, esperado %v", resultado, esperado)
+		}
 	}
+
+	t.Run("Soma o resto dos slices tirando a primeira posicao", func(t *testing.T) {
+
+		resultado := SomaTodoOResto([]int{1, 2}, []int{0, 9})
+		esperado := []int{2, 9}
+		verificaSomas(t, resultado, esperado)
+
+	})
+	t.Run("Soma o resto dos slices tirando a primeira posicao - 1 slice vazio", func(t *testing.T) {
+
+		resultado := SomaTodoOResto([]int{}, []int{0, 9})
+		esperado := []int{0, 9}
+
+		verificaSomas(t, resultado, esperado)
+	})
 }
